5d40728ac51f35c1decf8571dee469fcd4ce3e8e
DUMITRAS <dmytrorysan@gmail.com>
2020-05-13 14:27:51 +0300

    nar

--- backend/server.js
+++ backend/server.js
@@ -36,10 +36,10 @@ app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../build"));
+  app.use("/", express.static("../build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build/index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
