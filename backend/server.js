c67f246d431840e212d49b08ac49fd43be0e2aee
DUMITRAS <dmytrorysan@gmail.com>
2020-05-13 06:51:46 +0300

    omggg

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("../build"));
 
   app.get("*", (req, res) => {
-    res.sendFail(path.join(__dirname, "../build/index.html"));
+    res.sendFile(path.join(__dirname, "../build/index.html"));
   });
 }
 
