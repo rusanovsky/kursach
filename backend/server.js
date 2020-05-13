300faa9565e49b5d66d4f63e43de557fb0a512eb
DUMITRAS <dmytrorysan@gmail.com>
2020-05-13 05:12:00 +0300

    p

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -10,7 +11,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://Shokoman:Shokoman@cluster0-cvqcf.mongodb.net/test?retryWrites=true&w=majority";
+const uri =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://Shokoman:Shokoman@cluster0-cvqcf.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -18,7 +21,7 @@ mongoose.connect(uri, {
 });
 const connection = mongoose.connection;
 connection.once("open", () => {
-  console.log("MongoDB connected ");
+  console.log("MongoDB connected");
 });
 
 const exercisesRouter = require("./routes/exercises");
@@ -27,13 +30,13 @@ const usersRouter = require("./routes/users");
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("../build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../build"));
 
-//   app.get("*", (req, res) => {
-//     res.sendFail(path.join(__dirname, "../build/index.html"));
-//   });
-// } 
+  app.get("*", (req, res) => {
+    res.sendFail(path.join(__dirname, "../build/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
