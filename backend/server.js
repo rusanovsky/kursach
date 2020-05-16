d43090bc056a5c54c04adf0750502e6035b41eef
DUMITRAS <dmytrorysan@gmail.com>
2020-05-16 23:54:35 +0300

    try

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = "mongodb+srv://Shokoman:Shokoman@cluster0-cvqcf.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(
   uri,
   {
